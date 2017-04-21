687d7761bcdd8563100617c2d5858320d160fe91
ArpanGulrajani <sahilchanchlanimarch@gmail.com>
2017-04-21 23:47:41 +0530

    first version

---
+++ MmechV/app/src/main/java/com/example/vaibhav/mmechv/ReviewActivity.java
@@ -0,0 +1,58 @@
+package com.example.sahil.mobilemechanic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import static com.example.sahil.mobilemechanic.R.styleable.View;
+
+public class ReviewActivity extends AppCompatActivity {
+
+    EditText etName, etEmail, etReview;
+    TextView tvRating;
+    Button btSubmit;
+    RatingBar ratingBar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etReview = (EditText) findViewById(R.id.etReview);
+        tvRating = (TextView) findViewById(R.id.tvRating);
+        btSubmit = (Button) findViewById(R.id.btSubmit);
+
+        btSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = etName.getText().toString();
+                String Location = etLocation.getText().toString();
+                String email = etEmail.getText().toString();
+
+                if (etName.getText().toString().length() == 0) {
+                    etName.setError("Name Required");
+                    etName.requestFocus();
+                } else (!Patterns.EMAIL_ADDRESS.matcher(etEmail.getText().toString()).matches())
+                {
+                    etEmail.setError("Invalid Email ID");
+                    etEmail.requestFocus();
+                }
+
+                Intent i=new Intent(ReviewActivity.this,BlankActivity.class);
+                startActivity (i);
+
+            }
+        });
+    }
+}
+
